4131e4a2555d7892ab94cdd3e526a002d6228513
Peter Beverloo <peter@lvp-media.com>
2019-04-16 23:23:27 +0100

    Add an Environment object loading the year

--- src/app/Environment.tsx
+++ src/app/Environment.tsx
@@ -7,18 +7,56 @@ import { EnvironmentConfigPath } from '../config';
 // Represents the environment under which the volunteer portal is operating. Exposes various
 // configuration options unique to groups of volunteers.
 class Environment {
-    available : boolean = false;
+    available: boolean = false;
+    year?: number;
 
     // Initialization. Will fetch the environment configuration file from the server on pageload.
     // Expensive, but critical to the portal's behaviour.
     async initialize() {
-        // TODO: Load and parse the |EnvironmentConfigPath|.
+        try {
+            const result = await fetch(EnvironmentConfigPath);
+            if (!result.ok) {
+                console.error('Unable to fetch the environment configuration.');
+                return;
+            }
+
+            const config = JSON.parse(await result.text());
+            if (!config) {
+                console.error('Unable to parse the environment configuration.');
+                return;
+            }
+
+            if (!this.validateAndParseNumber(config, 'year')) {
+                console.error('Unable to parse the year from the environment configuration.');
+                return;
+            }
+
+            // Mark the environment as being available, as all validation has passed.
+            this.available = true;
+
+        } catch (e) {
+            console.error('Unable to load the environment configuration.', e);
+        }
     }
 
     // Returns whether the environment is readily available, meaning that it was successfully
     // loaded, parsed and verified.
     isAvailable() : boolean {
         return this.available;
+    }
+
+    // Validates the given |field| in the given |config| as a number. The |field| will be written
+    // to an identically named property in this class instance when successful.
+    private validateAndParseNumber(config: any, field: string): boolean {
+        if (!config.hasOwnProperty(field))
+            return false;
+
+        const value = Number(config[field]);
+        if (isNaN(value))
+            return false;
+
+        (this as any)[field] = value;
+        return true;
     }
 
 }
