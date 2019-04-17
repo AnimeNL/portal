2fe78a1945bba1e6be0321051d40faa2d4026668
Peter Beverloo <peter@lvp-media.com>
2019-04-17 01:03:55 +0100

    Change the theme color to indigo

--- src/config.tsx
+++ src/config.tsx
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the MIT license, a copy of which can
 // be found in the LICENSE file.
 
-import green from '@material-ui/core/colors/green';
+import indigo from '@material-ui/core/colors/indigo';
 
 const root = process.env.PUBLIC_URL;
 
@@ -12,7 +12,7 @@ export const EnvironmentConfigPath = root + '/data/environment.json';
 // Documentation: https://material-ui.com/customization/themes/
 export const AppTheme = {
     palette: {
-        primary: green,
+        primary: indigo,
     },
     typography: {
         useNextVariants: true,
